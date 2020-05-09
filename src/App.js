61ae0c1020a4a3c79c0ee2d19e011b92ffd149c7
mrnguyener21 <victortnguyen.workemail@gmail.com>
2020-05-08 22:15:40 -0700

    planned out more of the app and added required formulas

--- src/App.js
+++ src/App.js
@@ -4,35 +4,41 @@ import Select from 'react-select';
 
 const App = () => {
   const [initialInvestment, setInitialInvestment]= useState('');
-  const [monthlyContribution, setMonthlyContribution] =useState('');
   const [years,setYears] =useState('');
   const [interestRate, setInterestRate] =useState('');
 
 
   const options = [
     {value:'1' , label:'Annual'},
-    {value:'1/2' , label:'Semi-Annual'},
-    {value:'1/12' , label:'Monthly'},
+    {value:'2' , label:'Semi-Annual'},
+    {value:'12' , label:'Monthly'},
   ]
+
+  const interestInDecimals = interestRate/100;
+  const totalInterest = (initialInvestment*interestInDecimals)*(options*years);
+  const futureValueWithInterest = initialInvestment + totalInterest;
+  const FuturevalueWithCompoundInterest =Math.pow(initialInvestment*(1+interestInDecimals/options),options*years);
+  const totalCompoundInterest = FuturevalueWithCompoundInterest - initialInvestment;
   return(
     <div>
       <div>
-          <p>STEP 1 INITIAL INVESTMENT</p>
-          <input type={'text'} placeholder={'Inital Investment'} value={initialInvestment}  onChange={(e)=>setInitialInvestment(e.target.value)} />
+        <div>
+            <p>STEP 1 INITIAL INVESTMENT</p>
+            <input type={'text'} placeholder={'Inital Investment'} value={initialInvestment}  onChange={(e)=>setInitialInvestment(e.target.value)} />
+        </div>
+        <div>
+            <p>STEP 2 CONTRIBUTE</p>
+=            <input type={'text'} placeholder={'Years Invested'} value={years}  onChange ={(e)=>setYears(e.target.value)} />
+        </div>
+        <div>
+            <p>STEP 3 INTEREST RATE</p>
+            <input type={'text'} placeholder={'Interest Rate'} value={interestRate} onChange={(e)=>setInterestRate(e.target.value)} />
+        </div>
+        <div>
+            <p>STEP 4 COMPOUND IT</p>
+            <Select options={options}/>
+        </div>    
       </div>
-      <div>
-          <p>STEP 2 CONTRIBUTE</p>
-          <input type={'text'} placeholder={'Monthly Contribution'} value={monthlyContribution}  onChange={(e)=>setMonthlyContribution(e.target.value)}  />
-          <input type={'text'} placeholder={'Years Invested'} value={years}  onChange ={(e)=>setYears(e.target.value)} />
-      </div>
-      <div>
-          <p>STEP 3 INTEREST RATE</p>
-          <input type={'text'} placeholder={'Interest Rate'} value={interestRate} onChange={(e)=>setInterestRate(e.target.value)} />
-      </div>
-      <div>
-          <p>STEP 4 COMPOUND IT</p>
-          <Select options={options}/>
-      </div>    
       <Chart/>
     </div>
   )
--- src/readme.md
+++ src/readme.md
@@ -5,25 +5,42 @@ LAYOUT
 -step 1: initial investment
     -inputs: initial investment*
 -step 2: contribute
-    -inputs: monthly contribution, length of time in years*
+    -inputs: length of time in years*
 -step 3: interst rate
     -inputs: estimated interest rate*
 -step 4: compound it
-    -inputs: compound frequency(annually, semiannualy, monthly, daily)
+    -reactselect: compound frequency(annually, semiannualy, monthly, daily)
 
 
 PROJECT LAYOUT
 App
 -contain the inputs
+-need variables for compounded interest, amount + contributions, amount + contribution + compound interest
 
 -
 Chart
 -gonna need to receive the input data from the app 
 -have a calculate button which shows the graph
+-What to show on chart: initial invest, total with interest but not compounded, total with commpounding interest
 
-FORMUALA
-compound interst
--amount = initial principal(1+interst rate/number of times interest applied per time period)^number of times interest applied per time period * number of time periods elapsed
+FORMULA
+https://www.thecalculatorsite.com/articles/finance/compound-interest-formula.php
+A = the future value of the investment/loan, including interest
+P = the principal investment amount (the initial deposit or loan amount)
+r = the annual interest rate (decimal)
+n = the number of times that interest is compounded per unit t
+t = the time the money is invested or borrowed for
+
+compound interest(this will show the total amount earned)
+A = P (1 + r/n)^(nt)
+
+compound interest(only the interest earned)
+A = P (1 + r/n)^(nt) - P
+
+compound interst with monthly contributions
+A = (P+(M*12)^t)(1 + r/n)^(nt)
+
+
 
 EXAMPLE FROM STACK OVERFLOW
 https://stackoverflow.com/questions/4712585/calculating-future-value-with-compound-interest-with-javascript
